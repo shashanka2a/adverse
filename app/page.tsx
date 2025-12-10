4a422a9764a968be422132ed14aff214f8d6efec
shashank <jagannathamshashank@gmail.com>
2025-12-10 12:53:16 +0530

    up:2

--- app/page.tsx
+++ app/page.tsx
@@ -117,11 +117,7 @@ export default function HomePage() {
     const frameDisplay = frameRef.current;
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smooth: true,
-      touchMultiplier: 1.5
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
     });
 
     lenis.on("scroll", ScrollTrigger.update);
